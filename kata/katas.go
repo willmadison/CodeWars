5c233d54333d6e49abdd708caaede996fbad0bdd
Will Madison <wmadison@Wills-MacBook-Pro.local>
2018-11-15 16:30:55 -0500

    adding a few more go(lang) solutions

--- kata/katas.go
+++ kata/katas.go
@@ -2,8 +2,11 @@ package kata
 
 import (
 	"bytes"
+	"math"
 	"sort"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 func TwoOldestAges(ages []int) [2]int {
@@ -104,4 +107,232 @@ func DNAStrand(sequence string) string {
 	}
 
 	return b.String()
+}
+
+func FindShort(words string) int {
+	minLength := math.MaxInt64
+
+	for _, word := range strings.Fields(words) {
+		length := len(word)
+		if length < minLength {
+			minLength = length
+		}
+	}
+
+	return minLength
+}
+
+func Accum(expression string) string {
+	var buf bytes.Buffer
+
+	for i, l := range expression {
+		upper, lower := unicode.ToUpper(l), unicode.ToLower(l)
+
+		for j := 0; j < i+1; j++ {
+			if j == 0 {
+				buf.WriteRune(upper)
+				continue
+			}
+
+			buf.WriteRune(lower)
+		}
+
+		if i < len(expression)-1 {
+			buf.WriteRune('-')
+		}
+	}
+
+	return buf.String()
+}
+
+func Potatoes(p0, w0, p1 int) int {
+	return w0 * (100.0 - p0) / (100 - p1)
+}
+
+func ToNato(words string) string {
+	nato := []string{"Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"}
+
+	runeToNato := mapRunesToNato(nato)
+
+	var buf bytes.Buffer
+
+	for _, c := range words {
+		switch {
+		case unicode.IsLetter(c):
+			buf.WriteString(runeToNato[unicode.ToLower(c)])
+		case unicode.IsSpace(c):
+			continue
+		default:
+			buf.WriteRune(c)
+		}
+
+		buf.WriteRune(' ')
+	}
+
+	return strings.TrimSpace(buf.String())
+}
+
+func mapRunesToNato(nato []string) map[rune]string {
+	runeToNato := map[rune]string{}
+
+	for i, n := range nato {
+		runeToNato[rune('a'+i)] = n
+	}
+
+	return runeToNato
+}
+
+func ValidateSolution(board [][]int) bool {
+	return cellsAreValid(board) &&
+		rowsAreValid(board) &&
+		columnsAreValid(board)
+}
+
+func cellsAreValid(board [][]int) bool {
+	for cell := 1; cell < 10; cell++ {
+		valid := validateCell(cell, board)
+
+		if !valid {
+			return false
+		}
+	}
+
+	return true
+}
+
+func validateCell(cell int, board [][]int) bool {
+	rowStart, rowEnd := getRowBounds(cell)
+	colStart, colEnd := getColBounds(cell)
+
+	seen := make([]bool, 9)
+
+	for row := rowStart; row <= rowEnd; row++ {
+		for col := colStart; col <= colEnd; col++ {
+			v := board[row][col]
+
+			if v == 0 || seen[v-1] {
+				return false
+			}
+
+			seen[v-1] = true
+
+		}
+	}
+
+	for _, present := range seen {
+		if !present {
+			return false
+		}
+	}
+
+	return true
+}
+
+func getRowBounds(cell int) (int, int) {
+	switch {
+	case 1 <= cell && cell <= 3:
+		return 0, 2
+	case 4 <= cell && cell <= 6:
+		return 3, 5
+	case 7 <= cell && cell <= 9:
+		return 6, 8
+	default:
+		return -1, -1
+	}
+}
+
+func getColBounds(cell int) (int, int) {
+	switch {
+	case cell == 1 || cell == 4 || cell == 7:
+		return 0, 2
+	case cell == 2 || cell == 5 || cell == 8:
+		return 3, 5
+	case cell == 3 || cell == 6 || cell == 9:
+		return 6, 8
+	default:
+		return -1, -1
+	}
+}
+
+func rowsAreValid(board [][]int) bool {
+	for _, row := range board {
+		seen := make([]bool, 9)
+
+		for _, v := range row {
+			if v == 0 || seen[v-1] {
+				return false
+			}
+
+			seen[v-1] = true
+		}
+
+		for _, present := range seen {
+			if !present {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func columnsAreValid(board [][]int) bool {
+	for row := 0; row < len(board); row++ {
+		seen := make([]bool, 9)
+
+		for col := 0; col < len(board[row]); col++ {
+			v := board[row][col]
+
+			if v == 0 || seen[v-1] {
+				return false
+			}
+
+			seen[v-1] = true
+		}
+
+		for _, present := range seen {
+			if !present {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func Solution(numbers []int) string {
+	var buf bytes.Buffer
+	var i int
+
+	for i < len(numbers) {
+		buf.WriteString(strconv.Itoa(numbers[i]))
+
+		var rangeStart, rangeEnd int
+
+		if i < len(numbers)-1 && numbers[i+1]-numbers[i] == 1 {
+			rangeStart = i
+
+			for numbers[i+1]-numbers[i] == 1 {
+				i++
+
+				if i == len(numbers)-1 {
+					break
+				}
+			}
+
+			rangeEnd = i
+		}
+
+		if rangeEnd-rangeStart >= 2 { // Valid range must be at least three numbers...
+			buf.WriteRune('-')
+			buf.WriteString(strconv.Itoa(numbers[rangeEnd]))
+		} else if rangeEnd != 0 {
+			i = rangeStart
+		}
+
+		buf.WriteRune(',')
+		i++
+	}
+
+	return strings.TrimRight(buf.String(), ",")
 }
--- kata/katas_test.go
+++ kata/katas_test.go
@@ -214,3 +214,177 @@ func TestDNAStrand(t *testing.T) {
 		})
 	}
 }
+
+func TestFindShort(t *testing.T) {
+	tests := []struct {
+		given string
+		want  int
+	}{
+		{
+			"bitcoin take over the world maybe who knows perhaps",
+			3,
+		},
+		{
+			"turns out random test cases are easier than writing out basic ones",
+			3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("FindShort(%v)", tt.given), func(t *testing.T) {
+			if got := FindShort(tt.given); !assert.Equal(t, tt.want, got) {
+				t.Errorf("FindShort(%v)= %v, want %v", tt.given, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAccum(t *testing.T) {
+	tests := []struct {
+		given string
+		want  string
+	}{
+		{
+			"ZpglnRxqenU",
+			"Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu",
+		},
+		{
+			"NyffsGeyylB",
+			"N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("Accum(%v)", tt.given), func(t *testing.T) {
+			if got := Accum(tt.given); !assert.Equal(t, tt.want, got) {
+				t.Errorf("Accum(%v)= %v, want %v", tt.given, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPotatoes(t *testing.T) {
+	tests := []struct {
+		given struct {
+			p0, w0, p1 int
+		}
+		want int
+	}{
+		{
+			struct {
+				p0, w0, p1 int
+			}{99, 100, 98},
+			50,
+		},
+		{
+			struct {
+				p0, w0, p1 int
+			}{82, 127, 80},
+			114,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("Potatoes(%v)", tt.given), func(t *testing.T) {
+			if got := Potatoes(tt.given.p0, tt.given.w0, tt.given.p1); !assert.Equal(t, tt.want, got) {
+				t.Errorf("Potatoes(%v)= %v, want %v", tt.given, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestToNato(t *testing.T) {
+	tests := []struct {
+		given string
+		want string
+	}{
+		{
+			"If you can read",
+			"India Foxtrot Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta",
+		},
+		{
+			"Did not see that coming",
+			"Delta India Delta November Oscar Tango Sierra Echo Echo Tango Hotel Alfa Tango Charlie Oscar Mike India November Golf",
+		},
+		{
+			"go for it!",
+			"Golf Oscar Foxtrot Oscar Romeo India Tango !",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("ToNato(%v)", tt.given), func(t *testing.T) {
+			if got := ToNato(tt.given); !assert.Equal(t, tt.want, got) {
+				t.Errorf("ToNato(%v)= %v, want %v", tt.given, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidateSudokuSolution(t *testing.T) {
+	tests := []struct {
+		given [][]int
+		want bool
+	}{
+		{
+			[][]int{
+				{5, 3, 4, 6, 7, 8, 9, 1, 2},
+				{6, 7, 2, 1, 9, 5, 3, 4, 8},
+				{1, 9, 8, 3, 4, 2, 5, 6, 7},
+				{8, 5, 9, 7, 6, 1, 4, 2, 3},
+				{4, 2, 6, 8, 5, 3, 7, 9, 1},
+				{7, 1, 3, 9, 2, 4, 8, 5, 6},
+				{9, 6, 1, 5, 3, 7, 2, 8, 4},
+				{2, 8, 7, 4, 1, 9, 6, 3, 5},
+				{3, 4, 5, 2, 8, 6, 1, 7, 9},
+			},
+			true,
+		},
+		{
+			[][]int{
+				{5, 3, 4, 6, 7, 8, 9, 1, 2},
+				{6, 7, 2, 1, 9, 0, 3, 4, 8},
+				{1, 0, 0, 3, 4, 2, 5, 6, 0},
+				{8, 5, 9, 7, 6, 1, 0, 2, 0},
+				{4, 2, 6, 8, 5, 3, 7, 9, 1},
+				{7, 1, 3, 9, 2, 4, 8, 5, 6},
+				{9, 0, 1, 5, 3, 7, 2, 1, 4},
+				{2, 8, 7, 4, 1, 9, 6, 3, 5},
+				{3, 0, 0, 4, 8, 1, 1, 7, 9},
+			},
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("ValidateSolution(%v)", tt.given), func(t *testing.T) {
+			if got := ValidateSolution(tt.given); !assert.Equal(t, tt.want, got) {
+				t.Errorf("ValidateSolution(%v)= %v, want %v", tt.given, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolution(t *testing.T) {
+	tests := []struct {
+		given []int
+		want string
+	}{
+		{
+			[]int{-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20},
+			"-6,-3-1,3-5,7-11,14,15,17-20",
+		},
+		{
+			[]int{78, 79, 81, 82, 86, 87, 93, 97, 104, 110, 111, 112, 119, 124, 125, 129, 130, 131, 138},
+			"78,79,81,82,86,87,93,97,104,110-112,119,124,125,129-131,138",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("Solution(%v)", tt.given), func(t *testing.T) {
+			if got := Solution(tt.given); !assert.Equal(t, tt.want, got) {
+				t.Errorf("Solution(%v)= %v, want %v", tt.given, got, tt.want)
+			}
+		})
+	}
+}
